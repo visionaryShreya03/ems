a23a0a69d8154a7eef663525dcf6b84dcc218b21
visionaryShreya03 <shreya6302pandey@gmail.com>
2025-01-24 15:35:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ems/', // Add this line
   plugins: [react()],
 })
 

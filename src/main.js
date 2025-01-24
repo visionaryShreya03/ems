dd77523dc2f560c8c23e6038d4cd0e61a0593df4
visionaryShreya03 <shreya6302pandey@gmail.com>
2025-01-24 16:40:04 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ import AuthProvider from './context/AuthProvider.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   
-     <AuthProvider>
-     <App />
-     </AuthProvider>
-     
-  </StrictMode>,
+<React.StrictMode>
+
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+
+  </React.StrictMode>,
 )
